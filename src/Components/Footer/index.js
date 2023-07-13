87f681e34996a5b48bcde35322cae9396fb6b57e
ahmad-ahmadnejad <ahmad.ahmadnejad.1999@gmail.com>
2023-07-13 14:59:01 +0330

    create footer component

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,54 @@
+import  { InstagramOutlined,TwitterOutlined,MailOutlined } from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom';
+const Footer = () => {
+    const navigate = useNavigate();
+
+    const navigateToBlogsHandler = () => { 
+        navigate('/blogs')
+    }
+
+
+    return ( 
+        <div className="fixed bottom-0 left-0 w-full flex flex-row-reverse justify-between items-center p-4 text-white " style={{backgroundImage:`url(${'/images/footer_back.jpg'})`}}>
+
+                <div className="flex flex-col justify-center items-center gap-4 min-w-[200px] text-center" >
+                    <h2 className="font-bold text-xl">دسترسی آسان</h2>
+                    <h4>پرسش های متداول</h4>
+                    <h4>قوانین ترخینه</h4>
+                    <h4>حریم خصوصی</h4>
+                    <div className='flex flex-row justify-center items-center gap-2'>
+                    <InstagramOutlined />
+                    <TwitterOutlined/>
+                    <MailOutlined />
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-4 min-w-[200px] text-center" >
+                    <h2 className="font-bold text-xl">دسترسی آسان</h2>
+                    <h4>پرسش های متداول</h4>
+                    <h4>قوانین ترخینه</h4>
+                    <h4 onClick={navigateToBlogsHandler} className='cursor-pointer'>وبلاگ ترخینه </h4>
+                    <div className='flex flex-row justify-center items-center gap-2'>
+                    <InstagramOutlined />
+                    <TwitterOutlined/>
+                    <MailOutlined />
+                    </div>
+                </div>
+                <div>
+                    <h2>شعبه های ترخینه </h2>
+                    <h4> شعبه اکباتان </h4>
+                    <h4>شعبه چالوس </h4>
+                    <h4>شعبه اقدسیه</h4>
+                </div> 
+                <div>
+                    <h2>شعبه های ترخینه </h2>
+                    <h4> شعبه اکباتان </h4>
+                    <h4>شعبه چالوس </h4>
+                    <h4>شعبه اقدسیه</h4>
+                </div>               
+
+
+        </div>
+     );
+}
+ 
+export default Footer;
