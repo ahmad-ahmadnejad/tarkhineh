e7ad7178fe2297698b1afa3109e9aa628155c743
ahmad-ahmadnejad <ahmad.ahmadnejad.1999@gmail.com>
2023-07-13 15:00:19 +0330

    Add React-Router To Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import {  Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return ( 
     <>
+    {/* 
+    slider */}
     <Header/>
-    slider
+      <Routes>
+          <Route index element={<h1>Home</h1>} />
+          <Route path="/blogs" element={<h1>Blogs</h1>} />
+      </Routes>
+    <Footer/>
     </>
    );
 }
